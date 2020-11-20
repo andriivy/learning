db4304712208debdbe3fbd2a1109375dbfdaad35
andrii.shurda <andrii.shurda@gmail.com>
2020-11-20 16:43:15 +0200

    add task Piramida (2 days)

---
+++ src/com/company/Piramida.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Write a program to produce on console the following :
+ * 1
+ * 2 1
+ * 3 2 1
+ * 4 3 2 1
+ * 5 4 3 2 1
+ * 6 5 4 3 2 1
+ * 7 6 5 4 3 2 1
+ * 8 7 6 5 4 3 2 1
+ */
+public class Piramida {
+}
