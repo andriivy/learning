67606790d0ca4e1e22b241b517f5fd9e0d38bda1
andrii.vykhrystiuk <vikhristyk@gmail.com>
2020-11-23 01:31:13 +0200

    program for piramid building

--- src/com/company/Piramida.java
+++ src/com/company/Piramida.java
@@ -12,4 +12,18 @@ package com.company;
  * 8 7 6 5 4 3 2 1
  */
 public class Piramida {
+
+    public static void main(String[] args) {
+        int[] x = new int[8];
+        int i;
+        int y = 1;
+        String LineOfPiramid = "";
+
+        for (i = 0; i < x.length; i++) {
+            x[i] = y;
+            y++;
+            LineOfPiramid = x[i] + " " + LineOfPiramid;
+            System.out.println(LineOfPiramid);
+        }
+    }
 }
