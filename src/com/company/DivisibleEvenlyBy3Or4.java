9f5351052b284fa0c9f93b51b53d1afdc661d381
andrii.shurda <andrii.shurda@gmail.com>
2020-11-27 11:58:01 +0200

    add divisible evenly by 3 or 4 (2-6 hours)

---
+++ src/com/company/DivisibleEvenlyBy3Or4.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * The program must print the first 10 numbers that is divisible evenly by 3 or 4,
+ * but for loop iterates from 1 to 300 (so break statement must be used after 10 printed result).
+ * The output shall look: 3 4 … 20
+ */
+public class DivisibleEvenlyBy3Or4 {
+}
